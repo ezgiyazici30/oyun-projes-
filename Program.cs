6cdc74095c140672e8b0da2f10f7195e21f5b1a5
ezgiyazici30 <ezgiyazici3011@gmail.com>
2024-11-21 14:56:43 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("bir sayı giriniz:");
+int sayi= Convert.ToInt32(Console.ReadLine());
+int sonuc = sayi * sayi;
+Console.WriteLine("sayının karesi:"+sonuc);
+Console.ReadKey();
